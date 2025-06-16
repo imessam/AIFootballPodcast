6f7dc41999a7d13aa5701f808fe06e21993c8210
Mohamed Essam <imessam1998@gmail.com>
2025-06-16 23:59:55 +0300

    Enhance project structure by updating .gitignore, adding python-dotenv to requirements, and implementing session management in utils.py

---
+++ modules/utils.py
@@ -0,0 +1,121 @@
+import logging
+
+logging.basicConfig(level=logging.ERROR)
+
+from google.adk.agents import Agent
+from google.adk.sessions import InMemorySessionService
+from google.adk.runners import Runner
+from google.genai import types # For creating message Content/Parts
+
+async def create_session(app_name : str, 
+                        user_id : str, 
+                        session_id : str,
+                        initial_state = {}) -> InMemorySessionService:
+  
+
+    # @title Setup Session Service and Runner
+
+    # --- Session Management ---
+    # Key Concept: SessionService stores conversation history & state.
+    # InMemorySessionService is simple, non-persistent storage for this tutorial.
+    session_service = InMemorySessionService()
+
+    # Create the specific session where the conversation will happen
+    session = await session_service.create_session(
+        app_name=app_name,
+        user_id=user_id,
+        session_id=session_id,
+        state=initial_state
+    )
+    print(f"Session created: App='{app_name}', User='{user_id}', Session='{session_id}'")
+
+    
+    # Verify the initial state was set correctly
+    retrieved_session = await session_service.get_session(app_name=app_name,
+                                                            user_id=user_id,
+                                                            session_id = session_id)
+
+    print("\n--- Initial Session State ---")
+    if retrieved_session:
+        print(retrieved_session.state)
+    else:
+        print("Error: Could not retrieve session.")
+
+    return session_service
+
+
+def create_runner(agent : Agent, 
+                  session_service : InMemorySessionService, 
+                  app_name : str) -> Runner:
+
+    # --- Runner ---
+    # Key Concept: Runner orchestrates the agent execution loop.
+    runner = Runner(
+        agent=agent, # The agent we want to run
+        app_name=app_name,   # Associates runs with our app
+        session_service=session_service # Uses our session manager
+    )
+    print(f"Runner created for agent '{runner.agent.name}'.")
+
+    return runner
+
+# @title Define Agent Interaction Function
+async def call_agent_async(query: str, runner : Runner, user_id, session_id) -> str | None:
+  """Sends a query to the agent and prints the final response."""
+  print(f"\n>>> User Query: {query}")
+
+  # Prepare the user's message in ADK format
+  content = types.Content(role='user', parts=[types.Part(text=query)])
+
+  final_response_text = "Agent did not produce a final response." # Default
+
+  # Key Concept: run_async executes the agent logic and yields Events.
+  # We iterate through events to find the final answer.
+  async for event in runner.run_async(user_id=user_id, session_id=session_id, new_message=content):
+      # You can uncomment the line below to see *all* events during execution
+    #   print(f"  [Event] Author: {event.author}, Type: {type(event).__name__}, Final: {event.is_final_response()}, Content: {event.content}")
+
+      # Key Concept: is_final_response() marks the concluding message for the turn.
+      if event.is_final_response():
+          if event.content and event.content.parts:
+             # Assuming text response in the first part
+             final_response_text = event.content.parts[0].text
+          elif event.actions and event.actions.escalate: # Handle potential errors/escalations
+             final_response_text = f"Agent escalated: {event.error_message or 'No specific message.'}"
+          # Add more checks here if needed (e.g., specific error codes)
+          break # Stop processing events once the final response is found
+
+  print(f"<<< Agent Response: {final_response_text}")
+
+  return final_response_text
+
+
+# @title Run the Initial Conversation
+
+# We need an async function to await our interaction helper
+async def run_conversation(runner : Runner, user_id : str, session_id : str):
+
+    await call_agent_async("Hi there! I'm Mohamed",
+                                       runner=runner,
+                                       user_id=user_id,
+                                       session_id=session_id)
+    
+    await call_agent_async("What is the weather like in London?",
+                                       runner=runner,
+                                       user_id=user_id,
+                                       session_id=session_id)
+
+    await call_agent_async("How about Paris?",
+                                       runner=runner,
+                                       user_id=user_id,
+                                       session_id=session_id) # Expecting the tool's error message
+
+    await call_agent_async("Tell me the weather in New York",
+                                       runner=runner,
+                                       user_id=user_id,
+                                       session_id=session_id)
+    
+    await call_agent_async("Farewell!",
+                                       runner=runner,
+                                       user_id=user_id,
+                                       session_id=session_id)
